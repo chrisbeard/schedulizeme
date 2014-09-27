0033495395e7423bbb2420d89fe4a9d895282990
Jack Fischer <jfische8@binghamton.edu>
2014-09-26 22:58:36 -0400

    DAMN bro sections api working

--- api/queries.php
+++ api/queries.php
@@ -1,8 +1,8 @@
 <?php
 
 $getDepartments = "SELECT DISTINCT dept FROM sections";
-$getClasses = "SELECT DISTINCT num FROM sections WHERE dept=:dept";
-$getSections = "SELECT section FROM sections WHERE dept=:dept' AND num=:num";
+$getClasses = "SELECT DISTINCT num, title FROM sections WHERE dept=:dept GROUP BY num";
+$getSections = "SELECT section, days, times, location, instructor FROM sections WHERE dept=:dept AND num=:num GROUP BY section";
 $getCRN = "SELECT crn FROM sections WHERE dept=:dept AND num=:num AND section=:section";
 
 // then after that you can get a whole bunch of data from the crn, for example:
--- api/sections.php
+++ api/sections.php
@@ -4,10 +4,11 @@ require('db.php');
 require('queries.php');
 
 $dept = $_GET["dept"];
+$course = $_GET["courseNum"];
 
-$STH = $DBH->prepare($getClasses);
-
-$STH->execute(array(':dept' => $dept));
+$STH = $DBH->prepare($getSections);
+$test = "\':dept\' => \$dept";
+$STH->execute(array(':dept' => $dept, ':num' => $course));
 
 $results=$STH->fetchAll(PDO::FETCH_ASSOC);
 $json=json_encode($results);
