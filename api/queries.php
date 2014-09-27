f34e2b39e139ebf09af9d95cdd4271fbc9ea5e2c
Emily Goetz <egoetz1@binghamton.edu>
2014-09-26 22:21:52 -0400

    Update queries.php

--- api/queries.php
+++ api/queries.php
@@ -3,5 +3,6 @@
 $getDepartments = "SELECT DISTINCT dept FROM sections";
 $getClasses = "SELECT DISTINCT num FROM sections WHERE dept='<dept name>'";
 $getSections = "SELECT section FROM sections WHERE dept='<dept name>' AND num=<class num>";
+$getCRN = "SELECT crn FROM sections WHERE dept='<dept name>' AND num=<class num> AND section='<sect num>'";
 
 ?>
