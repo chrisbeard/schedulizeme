763e3745c7bc92279205f8a4bf2562fe4259b67f
Emily Goetz <egoetz1@binghamton.edu>
2014-09-26 22:24:19 -0400

    Update queries.php

--- api/queries.php
+++ api/queries.php
@@ -5,4 +5,9 @@ $getClasses = "SELECT DISTINCT num FROM sections WHERE dept='<dept name>'";
 $getSections = "SELECT section FROM sections WHERE dept='<dept name>' AND num=<class num>";
 $getCRN = "SELECT crn FROM sections WHERE dept='<dept name>' AND num=<class num> AND section='<sect num>'";
 
+// then after that you can get a whole bunch of data from the crn, for example:
+$getTitle = "SELECT title FROM sections WHERE crn='<crn>'";
+
+
+
 ?>
